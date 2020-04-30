c0075c119c2ef65c69a0a5725f36e777d1134a5d
arkd <aaronmdewitt@gmail.com>
2020-04-30 16:25:42 -0500

    actual optimization work

--- code_skeleton/gemm_09.c
+++ code_skeleton/gemm_09.c
@@ -10,15 +10,13 @@
 int MC;
 int KC;
 
-#define alpha( i,j ) A[ (j)*ldA + (i) ]   // map alpha( i,j ) to array A
-#define beta( i,j )  B[ (j)*ldB + (i) ]   // map beta( i,j ) to array B
-#define gamma( i,j ) C[ (j)*ldC + (i) ]   // map gamma( i,j ) to array C
+#define alpha( a,i,j ) a[ (j)*ldA + (i) ]   // map alpha( i,j ) to array A
+#define beta( b,i,j )  b[ (j)*ldB + (i) ]   // map beta( i,j ) to array B
+#define gamma( c,i,j ) c[ (j)*ldC + (i) ]   // map gamma( i,j ) to array C
 
 #define min( x, y ) ( ( x ) < ( y ) ? x : y )
 
-/* Optimized packing by using aligned buffers */
-// double* Atilde = (double *) _mm_malloc(MC*KC*sizeof(double));
-// double* Btilde = (double *) _mm_malloc(KC*NC*sizeof(double));
+
 
 void PackMicroPanelA_MRxKC( int m, int k, double *A, int ldA, double *Atilde )
 /* Pack a micro-panel of A into buffer pointed to by Atilde.
@@ -79,5 +77,45 @@ void PackPanelB_KCxNC( int k, int n, double *B, int ldB, double *Btilde )
 void MyGemm(int m, int n, int k, double *A, int ldA,
             double *B, int ldB, double *C, int ldC)
 {
+    if ( m % MR != 0 || MC % MR != 0 ){
+        printf( "m and MC must be multiples of MR\n" );
+        exit( 0 );
+    }
+    if ( n % NR != 0 || NC % NR != 0 ){
+        printf( "n and NC must be multiples of NR\n" );
+        exit( 0 );
+    }
+    /* Optimized packing by using aligned buffers */
+     double* Atilde = (double *) _mm_malloc(MC*KC*sizeof(double));
+     double* Btilde = (double *) _mm_malloc(KC*NC*sizeof(double));
     /*loop5*/
+    for ( int j=0; j<n; j+=NC ) {
+        int njb = min(NC, n - j);  /* Last loop may not involve a full block */
+        double* Bj = &beta(B,0,j);  /* j column of B */
+        double* Cj = &gamma(c,0,j);  /* j column of C*/
+        /*loop4*/
+        for ( int p=0; p<k; p+=KC ) {
+            int kpb = min(KC, k - p);    /* Last loop may not involve a full block */
+            PackPanelB_KCxNC(kpb, njb, &beta(Bj, p, 0), ldB, Btilde);
+            double *Ap = &alpha(0, p);  /* column partition A */
+            /*loop3*/
+            for (int i = 0; i < m; i += MC) {
+                int mib = min(MC, m - i);    /* Last loop may not involve a full block */
+                PackBlockA_MCxKC(ib, pb, &alpha(Ap, i, 0), ldA, Atilde);
+                double *Cji = &gamma(Cj, i, 0); /* ith row partion of Cj */
+                /* loop2, n, m, & k are replaced by njb, mib, kpb here */
+                for ( int j2=0; j2<njb; j2+=NR ) {
+                    int jb = min(NR, njb - j2);
+                    double *MicroPanelB = &Btilde[j2 * kpb];
+                    double *Cjij2 = &gamma(Cji, 0, j2);
+                    /*loop1*/
+                    for (int i2 = 0; i2 < mib; i2 += MR) {
+                        int ib = min(MR, mib - i2);
+                        Gemm_MRxNRKernel_Packed(kpb, &Atilde[i2 * kpb],
+                            MicroPanelB, &gamma(Cjij2, i2, 0), ldC);
+                    }
+                }
+            }
+        }
+    }
 }
